25b1619a382f8d048688b9ddf4fe777e35c2cdf7
Jodi Jonatan <jdi102350@gmail.com>
2026-02-02 14:48:15 +0700

    env implementation

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -1,20 +1,30 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
+import 'package:flutter_dotenv/flutter_dotenv.dart'; // Import package
 import '../models/news_model.dart';
 
 class ApiService {
-  final String apiKey = "6dec5f74fc264a138d600bd1e3074c86";
-  final String baseUrl = "https://newsapi.org/v2/everything?q=tesla&apiKey=";
+  // Mengambil nilai langsung dari file .env
+  String get apiKey => dotenv.get('API_KEY', fallback: '');
+  String get baseUrl => dotenv.get('BASE_URL', fallback: '');
 
   Future<List<Article>> getNews() async {
-    final response = await http.get(Uri.parse(baseUrl + apiKey));
+    // Menyusun URL menggunakan variabel dari .env
+    // Kita tambahkan query parameter secara dinamis
+    final String fullUrl = "$baseUrl?q=tesla&apiKey=$apiKey";
 
-    if (response.statusCode == 200) {
-      Map<String, dynamic> json = jsonDecode(response.body);
-      List<dynamic> body = json['articles'];
-      return body.map((item) => Article.fromJson(item)).toList();
-    } else {
-      throw ("Gagal mengambil data berita");
+    try {
+      final response = await http.get(Uri.parse(fullUrl));
+
+      if (response.statusCode == 200) {
+        Map<String, dynamic> json = jsonDecode(response.body);
+        List<dynamic> body = json['articles'];
+        return body.map((item) => Article.fromJson(item)).toList();
+      } else {
+        throw ("Gagal mengambil data berita: ${response.statusCode}");
+      }
+    } catch (e) {
+      throw ("Terjadi kesalahan: $e");
     }
   }
 }
